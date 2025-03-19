bbbd1b1bf30195e2577da853b8a311295a4df740
Michał Czyż <mike@c2yz.com>
2025-03-19 01:26:39 +0100

    build refactor

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@
 
 use core::panic::PanicInfo;
 
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+use bootloader_api::{BootInfo, entry_point};
+
+entry_point!(encore_main);
+
+fn encore_main(_boot_info: &'static mut BootInfo) -> ! {
     loop {}
 }
 
